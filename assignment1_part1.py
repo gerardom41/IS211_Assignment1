d78dd6d7aa37f728f73c7d1f708f76ceb71da301
gerardom41 <gerardomolina9134@gmail.com>
2024-01-26 18:41:53 -0500

    Hw 1 parts 1 and 2
    list divide, assertion error
    class book, display

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,36 @@
 
-def list_divide(numbers, divide):
+class ListDivideException(Exception):
+    pass
+
+def list_divide(numbers, divide = 2):
     """
     The function returns the number of elements in the numbers list that are divisibleby divide
     """
-    pass
+    count =0
+    for num in numbers:
+        if num % divide == 0:
+            count += 1
+
+    print(f"{count} numbers divisible")
+    return count
 
 def test_list_divide():
     """
     Test listDivide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    try:
+        assert list_divide([1,2,3,4,5]) == 2
+        assert list_divide([2,4,6,8,10]) == 5
+        assert list_divide([30, 54, 63,98, 100], divide=10) == 2
+        assert list_divide([]) == 0
+        assert list_divide([1,2,3,4,5], 1) == 5
+
+    except AssertionError:
+        raise ListDivideException()
+
 if __name__ == "__main__":
-    testListDivide()
+    try:
+        test_list_divide()
+
+    except ListDivideException:
+        print(f"Not all tests passed")
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,14 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
+       self.author = author
+       self.title = title 
     
     def display(self):
-        pass
-
+        print(f'"{self.title}, written by {self.author}"')
 
 if __name__ == "__main__":
-    pass
+    book1 = Book("J.K Rowling", "Harry Potter and the Goblet of Fire")
+    book2 = Book("Walter Scoot", "Ivanhoe: A Romance")
+    book1.display()
+    book2.display()
